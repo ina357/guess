50ad52f36f1fec0e82f1dd838b9b838e15a07120
ina357 <nature10017@naver.com>
2022-06-06 13:10:38 +0900

    게스

--- mokup/mobile/js/script.js
+++
@@ -1,6 +0,0 @@
-const moreBtn=document.querySelector('.info .moreBtn');
-const title=document.querySelector('.info .metadata .title');
-moreBtn.addEventListener('click', ()=>{
-    title.classList.toggle('addClick');
-    moreBtn.classList.toggle('clicked');
-});
